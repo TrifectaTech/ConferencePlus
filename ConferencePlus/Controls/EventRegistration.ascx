922acdb001f73d90543c7279e6d70105ac5ba200
JDuverge <jduverge1020@gmail.com>
2013-12-09 14:07:34 -0500

    Clean Up and Add Untracked Files

---
+++ ConferencePlus/Controls/EventRegistration.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="EventRegistration.ascx.cs" Inherits="ConferencePlus.Controls.EventRegistration" %>
---
+++ ConferencePlus/Controls/EventRegistration.ascx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ConferencePlus.Base;
+
+namespace ConferencePlus.Controls
+{
+	public partial class EventRegistration : BaseControl
+	{
+		public int? EventId
+		{
+			get { return (int?)ViewState["CurrentEventId"]; }
+			set { ViewState["CurrentEventId"] = value; }
+		}
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		public void ReloadControl()
+		{		
+		}
+	}
+}
